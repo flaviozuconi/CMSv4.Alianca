b35c0e8a7d61b0d4ae353a79220a728128a14346
Larissa-Andrade <larissa_29.andrade@hotmail.com>
2022-04-27 13:20:49 -0300

    Tela de Gestão de Informações de Exportação

---
+++ CMSv4.Model/Base/GestaoInformacoesExportacao/MLGestaoInformacoesExportacao.cs
@@ -0,0 +1,37 @@
+﻿using Framework.Model;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+
+namespace CMSv4.Model.Base.GestaoInformacoesExportacao
+{
+    [Table("MOD_GIE_GESTAO_INFORMACOES_EXPORTACAO")]
+    [Serializable]
+    public class MLGestaoInformacoesExportacao
+    {
+        [DataField("GIE_N_CODIGO", SqlDbType.Decimal, 18, PrimaryKey = true, AutoNumber = true)]
+        public decimal? Codigo { get; set; }
+
+        [Required]
+        [CsvField("Proposta Comercial", 1)]
+        [DataField("GIE_C_PROPOSTA_COMERCIAL", SqlDbType.VarChar, 200)]
+        public string PropostaComercial { get; set; }
+
+        [Required]
+        [CsvField("Número do Booking", 2)]
+        [DataField("GIE_C_NUMERO_BOOKING", SqlDbType.VarChar, 200)]
+        public string NumeroBooking { get; set; }
+
+        [Required]
+        [DataField("GIE_D_DATA_ATUALIZACAO", SqlDbType.DateTime)]
+        public DateTime? DataAtualizacao { get; set; }
+
+        [JoinField("GIE_USU_N_CODIGO", "FWK_USU_USUARIO", "USU_N_CODIGO", "USU_C_NOME")]
+        [DataField("USU_C_NOME", SqlDbType.VarChar, 100)]
+        public string Usuario { get; set; }
+
+        [DataField("GIE_USU_N_CODIGO", SqlDbType.Decimal, 18)]
+        public decimal? CodigoUsuario { get; set; }
+    }
+}
---
+++ CMSv4.Model/Base/GestaoInformacoesExportacao/MLGestaoInformacoesExportacaoHistorico.cs
@@ -0,0 +1,30 @@
+﻿using Framework.Model;
+using System;
+using System.Data;
+
+namespace CMSv4.Model.Base.GestaoInformacoesExportacao
+{
+    [Table("MOD_GIE_GESTAO_INFORMACOES_EXPORTACAO_HISTORICO")]
+    public class MLGestaoInformacoesExportacaoHistorico
+    {
+        [DataField("IEH_N_CODIGO", SqlDbType.Decimal, 18, 0, PrimaryKey = true, AutoNumber = true)]
+        public decimal? Codigo { get; set; }
+
+        [DataField("IEH_D_DATA_IMPORTACAO", SqlDbType.DateTime)]
+        public DateTime? DataImportacao { get; set; }
+
+        [DataField("IEH_C_USUARIO", SqlDbType.VarChar, 100)]
+        public string Usuario { get; set; }
+
+        [DataField("IEH_B_SUCESSO", SqlDbType.Bit)]
+        public bool? Sucesso { get; set; }
+
+        [DataField("IEH_C_ARQUIVO", SqlDbType.VarChar, 200)]
+        public string Arquivo { get; set; }
+
+        [DataField("IEH_B_FINALIZADO", SqlDbType.Bit)]
+        public bool? Finalizado { get; set; }
+
+        
+    }
+}
