404adcb2d5c060c409fb7657131c59945e0f1670
William Henrique <William Henrique@DESKTOP-DSSFBDO>
2022-09-08 14:20:50 -0300

    update system

---
+++ CMSv4/Areas/ModuloAdmin/Controllers/LogIntegracaoAdminController.cs
@@ -0,0 +1,22 @@
+﻿using Framework.Utilities;
+using System.Web.Mvc;
+using CMSv4.Model;
+using CMSv4.BusinessLayer;
+
+namespace CMSApp.Areas.ModuloAdmin.Controllers
+{
+    public class AssuntosController : AdminBaseCRUDPortalController<MLAssuntos, MLAssuntos>
+    {
+        #region Item
+
+        [CheckPermission(global::Permissao.Modificar, ValidarModelState = true)]
+        [HttpPost]
+        public override ActionResult Item(MLAssuntos model)
+        {
+            TempData["Salvo"] = new BLAssunto().Salvar(model) > 0;
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+    }
+}
